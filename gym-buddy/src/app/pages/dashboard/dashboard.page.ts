5fa772c0c6f35c278f2de833e42ad0993859b072
MedinaPiRRy <medina.khe@gmail.com>
2025-03-28 12:58:29 -0400

    [Add] Created the dashboard page where all other pages will come together. Updated tabs routing module to include pages as new tabs; and deleted default 1 - 3 tabs created by IONIC

--- gym-buddy/src/app/pages/dashboard/dashboard.page.ts
+++ gym-buddy/src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class DashboardPage implements OnInit {
+  latestWorkout: any = null;
+  latestMetric: any = null;
+  activeGoals: any[] = [];
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.loadLatestWorkout();
+    this.loadLatestMetric();
+    this.loadActiveGoals();
+  }
+
+  loadLatestWorkout() {
+    this.api.getWorkouts().subscribe({
+      next: (res) => {
+        const sorted = res.message.sort(
+          (a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime()
+        );
+        this.latestWorkout = sorted[0];
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  loadLatestMetric() {
+    this.api.getMetrics().subscribe({
+      next: (res) => {
+        const sorted = res.message.sort(
+          (a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime()
+        );
+        this.latestMetric = sorted[0];
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  loadActiveGoals() {
+    this.api.getGoals().subscribe({
+      next: (res) => {
+        this.activeGoals = res.message.filter((g: any) => !g.isCompleted);
+      },
+      error: (err) => console.error(err),
+    });
   }
 
 }
--- gym-buddy/src/app/tabs/tabs-routing.module.ts
+++ gym-buddy/src/app/tabs/tabs-routing.module.ts
@@ -9,15 +9,19 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../pages/dashboard/dashboard.module').then(m => m.DashboardPageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../pages/goals/goals.module').then(m => m.GoalsPageModule)
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('../pages/workout/workout.module').then(m => m.WorkoutPageModule)
+      },
+      {
+        path: 'tab4',
+        loadChildren: () => import('../pages/progress/progress.module').then(m => m.ProgressPageModule)
       },
       {
         path: '',
